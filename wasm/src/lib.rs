3d2172a957986a58e5346356c0ff3d773e430a2e
Nyrox <root@nyrox.tech>
2020-07-15 06:37:13 +0200

    Changes to lib. Nothing implemented yet

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use renderjack;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -14,6 +15,23 @@ extern {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm!");
+#[derive(Copy, Clone, Debug)]
+pub struct Vector3f {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[wasm_bindgen]
+pub struct Tri3(Vector3f, Vector3f, Vector3f);
+
+#[wasm_bindgen]
+pub fn create_tri3(a: Vector3f) -> Tri3 {
+    Tri3(a, a, a)
+}
+
+#[wasm_bindgen]
+pub fn fullscreen_raster(width: usize, height: usize) -> Vec<u8> {
+
+    vec![]
 }
